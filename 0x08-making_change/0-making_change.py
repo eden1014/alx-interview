d3632296280810731f13c782773ad9454943cf4f
eden <hailueden14@gmail.com>
2023-05-18 07:07:07 -0700

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,9 +3,16 @@
     determine the fewest number of coins needed to meet
     a given amount total.
 '''
-import sys
+
+
 
 def makeChange(coins, total):
+    ''' 
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
+
     if total <= 0:
         return 0
 
